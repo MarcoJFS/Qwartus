c0cb2d1f3a4516a46bba1ac1165c78e2be176160
miguel12340 <miguelrochacalhau@hotmail.com>
2016-11-17 08:17:46 +0000

    ok
    ok

--- app/src/main/java/pt/ipleira/estg/qwartus/ac_Qwartus.java
+++ app/src/main/java/pt/ipleira/estg/qwartus/ac_Qwartus.java
@@ -10,13 +10,15 @@ import android.view.View.OnClickListener;
 
 public class ac_Qwartus extends AppCompatActivity {
 
-    Spinner spinnerDistrito = (Spinner) findViewById(R.id.spinnerDistrito);
-    Spinner spinnerConcelho = (Spinner) findViewById(R.id.spinnerConcelho);
+    //Spinner spinnerDistrito;
+    //Spinner spinnerConcelho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ac__qwartus);
+
+        //Spinner spinnerDistrito = (Spinner) findViewById(R.id.spinnerDistrito);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -26,30 +28,36 @@ public class ac_Qwartus extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinnerDistrito.setAdapter(adapter);
 
+
+        spinnerDistrito.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Spinner spinnerConcelho = (Spinner) findViewById(R.id.spinnerConcelho);
+
+                switch (position){
+                    case 1: // Create an ArrayAdapter using the string array and a default spinner layout
+                        ArrayAdapter<CharSequence> adapter0 = ArrayAdapter.createFromResource(getApplicationContext(),
+                                R.array.arrayConcelhosAveiro, android.R.layout.simple_spinner_item);
+                        // Specify the layout to use when the list of choices appears
+                        adapter0.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        // Apply the adapter to the spinner
+                        spinnerConcelho.setAdapter(adapter0);
+
+                    case 2: // Create an ArrayAdapter using the string array and a default spinner layout
+                        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getApplicationContext(),
+                                R.array.arrayConcelhosBeja, android.R.layout.simple_spinner_item);
+                        // Specify the layout to use when the list of choices appears
+                        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        // Apply the adapter to the spinner
+                        spinnerConcelho.setAdapter(adapter1);
+
+                }
+
+            }
+        });
+
     }
 
-    public void onItemSelected(AdapterView<ArrayAdapter> parent, View view, int position, long id) {
 
-        int pos = spinnerDistrito.getSelectedItemPosition();
-
-        switch (pos){
-            case 0: // Create an ArrayAdapter using the string array and a default spinner layout
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                        R.array.arrayConcelhosAveiro, android.R.layout.simple_spinner_item);
-                // Specify the layout to use when the list of choices appears
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                // Apply the adapter to the spinner
-                spinnerConcelho.setAdapter(adapter);
-                break;
-            case 1: // Create an ArrayAdapter using the string array and a default spinner layout
-                ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                        R.array.arrayConcelhosBeja, android.R.layout.simple_spinner_item);
-                // Specify the layout to use when the list of choices appears
-                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                // Apply the adapter to the spinner
-                spinnerConcelho.setAdapter(adapter1);
-                break;
-        }
-
-    }
 }
